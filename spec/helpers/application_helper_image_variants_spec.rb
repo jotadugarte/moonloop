114a8a9be22edd723e4f3f58abf73b6927cf5607
jotadugarte <97250038+jotadugarte@users.noreply.github.com>
2026-04-25 14:44:24 -0600

    test(images): stub image_tag in helper spec
    Stub ApplicationHelper#image_tag in the helper spec to avoid Propshaft asset lookup, while still asserting we request WebP variants via attachment.variant(format: :webp).

--- spec/helpers/application_helper_image_variants_spec.rb
+++ spec/helpers/application_helper_image_variants_spec.rb
@@ -13,6 +13,7 @@ RSpec.describe ApplicationHelper, type: :helper do
       allow(attachment).to receive(:variable?).and_return(true)
       allow(ImageVariants::Available).to receive(:call).and_return(true)
       allow(attachment).to receive(:variant).and_return("variant-ref")
+      allow(helper).to receive(:image_tag).and_return("<img>")
 
       helper.attachable_image_tag(attachment, resize_to_limit: ImageVariants::ResizeToLimit.for(:thumb), alt: "x")
 
